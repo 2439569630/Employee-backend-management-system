75c485cffff539115ac9666d26af19ed4ecedeb4
xgw <2439569630@qq.com>
2023-06-06 21:56:50 +0800

    1.0.1

---
+++ src/db/db.ts
@@ -0,0 +1,10 @@
+const server: { host: string } = {
+    host: '66'
+}
+// 冒泡排序算法 https://codegeex.cn 
+function bubbleSort<T>(a: T[], cmp: (a: T, b: T) => number): void {
+    
+}
+export default {
+    server
+}   
