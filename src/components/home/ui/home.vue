de51eeff2f6c8e5c5d6e3ee6092893a085c65fde
xgw <2439569630@qq.com>
2023-07-14 23:27:44 +0800

    添加上一次登录提示

--- src/components/home/ui/home.vue
+++ src/components/home/ui/home.vue
@@ -15,7 +15,46 @@
     import router from './../../../router/index';
     import heads from './../ui/head.vue'
     import left from './../ui/left.vue'
+    import { ElNotification } from 'element-plus'
+    import {getCurrentInstance, onBeforeMount} from 'vue'
+    // 全局配置
+    import url from './../../../db/db'
+    // 调用axios，使用全局的axios
+    const internalInstance = getCurrentInstance();
+    const instance = internalInstance.appContext.config.globalProperties.$axios;
+
+
+    // 注册一个钩子，在组件被挂载之前被调用。
+    onBeforeMount(() => {
+        instance.post(url.url + '/ues/loginRecord')
+            .then(res => {
+                ElNotification({
+                title: '上一次登录',
+                message: `
+                名称: ${res.data.name}<br>
+                登录数据：${res.data.loginTime}<br>
+                登录地点：${res.data.loginLocation}<br>
+                登录设备：${res.data.loginDevice}
+                `,
+                type: 'warning',
+                position: 'bottom-right',
+                duration: 0,
+                dangerouslyUseHTMLString: true,
+                customClass: 'mynotification'
+            })
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    })
 </script>
+<style>
+.mynotification {
+        width: 500px;
+        height: 200px;
+        overflow: auto;
+}
+</style>
 <style scoped>
     .head {
         /* width: 100vw; */
@@ -25,7 +64,6 @@
         top: 0;
         left: 12vw;
         right: 0;
-
     }
     .left {
         position: fixed;
@@ -45,4 +83,5 @@
         bottom: 0;
         background-color: #F5F5F5;
     }
+    
 </style>
